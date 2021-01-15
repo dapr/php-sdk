697c0251a1883cff98a1be3b5de61c257779d63b
Rob Landers <landers.robert@gmail.com>
2021-01-15 20:11:10 +0100

    Allow specifying on methods

--- src/lib/Deserialization/Attributes/ArrayOf.php
+++ src/lib/Deserialization/Attributes/ArrayOf.php
@@ -2,7 +2,7 @@
 
 namespace Dapr\Deserialization\Attributes;
 
-#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER)]
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_PARAMETER | \Attribute::TARGET_METHOD)]
 final class ArrayOf
 {
     public function __construct(public string $type)
