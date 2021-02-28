99c33f125fcc708efb68cbe1e0c47814dcb2afa5
Rob Landers <landers.robert@gmail.com>
2021-02-28 11:45:48 +0100

    Add tests

--- src/lib/Actors/Internal/Caches/FileCache.php
+++ src/lib/Actors/Internal/Caches/FileCache.php
@@ -6,32 +6,29 @@ namespace Dapr\Actors\Internal\Caches;
  * Class FileCache
  * @package Dapr\Actors\Internal\Caches
  */
-class FileCache implements CacheInterface {
+class FileCache implements CacheInterface
+{
     private array $data = [];
-
-    private function serialize_cache() {
-        $filename = sys_get_temp_dir().'/actor-cache/'.$this->cache_name;
-        file_put_contents($filename, serialize($this->data));
-    }
-
-    private function unserialize_cache() {
-        $filename = sys_get_temp_dir().'/actor-cache/'.$this->cache_name;
-        if(file_exists($filename)) {
-            $this->data = unserialize(file_get_contents($filename));
-        }
-    }
 
     public function __construct(private string $cache_name)
     {
-        if(!file_exists(sys_get_temp_dir().'/actor-cache')) {
+        if ( ! file_exists(sys_get_temp_dir().'/actor-cache')) {
             mkdir(sys_get_temp_dir().'/actor-cache');
         }
         $this->unserialize_cache();
     }
 
+    private function unserialize_cache()
+    {
+        $filename = sys_get_temp_dir().'/actor-cache/'.$this->cache_name;
+        if (file_exists($filename)) {
+            $this->data = unserialize(file_get_contents($filename));
+        }
+    }
+
     public function get_key(string $key): mixed
     {
-        if(array_key_exists($key, $this->data)) {
+        if (array_key_exists($key, $this->data)) {
             return $this->data[$key];
         }
         throw new KeyNotFound();
@@ -52,7 +49,22 @@ class FileCache implements CacheInterface {
         $this->data = [];
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
         $this->serialize_cache();
+    }
+
+    private function serialize_cache()
+    {
+        $filename = sys_get_temp_dir().'/actor-cache/'.$this->cache_name;
+        if ($this->data === []) {
+            if (file_exists($filename)) {
+                unlink($filename);
+            }
+
+            return;
+        }
+
+        file_put_contents($filename, serialize($this->data));
     }
 }
