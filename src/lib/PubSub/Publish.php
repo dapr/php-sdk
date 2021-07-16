7a911f3178756c0fa5fa9c23de76ecd83e4494e0
Rob Landers <landers.robert@gmail.com>
2021-07-16 15:23:45 +0200

    Update topic to use new pubsub

--- src/lib/PubSub/Publish.php
+++ src/lib/PubSub/Publish.php
@@ -5,18 +5,20 @@ namespace Dapr\PubSub;
 use DI\DependencyException;
 use DI\FactoryInterface;
 use DI\NotFoundException;
+use JetBrains\PhpStorm\Deprecated;
 
 /**
  * Class Publish
  * @package Dapr\PubSub
  */
+#[Deprecated(since: '1.2.0', replacement: Topic::class)]
 class Publish
 {
     /**
      * Publish constructor.
      *
      * @param string $pubsub
-     * @param FactoryInterface $container
+     * @param FactoryInterface|null $container
      */
     public function __construct(private string $pubsub, private FactoryInterface $container)
     {
--- src/lib/PubSub/Topic.php
+++ src/lib/PubSub/Topic.php
@@ -2,6 +2,7 @@
 
 namespace Dapr\PubSub;
 
+use Dapr\Client\DaprClient as NewClient;
 use Dapr\DaprClient;
 use Dapr\exceptions\DaprException;
 use Psr\Log\LoggerInterface;
@@ -15,8 +16,8 @@ class Topic
     public function __construct(
         private string $pubsub,
         private string $topic,
-        private DaprClient $client,
-        private LoggerInterface $logger
+        private DaprClient|NewClient $client,
+        private LoggerInterface|null $logger = null
     ) {
     }
 
@@ -29,7 +30,7 @@ class Topic
      *
      * @return bool Whether the event was successfully dispatched
      */
-    public function publish(mixed $event, ?array $metadata = null, $content_type = 'application/json'): bool
+    public function publish(mixed $event, ?array $metadata = null, string $content_type = 'application/json'): bool
     {
         $this->logger->debug('Sending {event} to {topic}', ['event' => $event, 'topic' => $this->topic]);
         if ($event instanceof CloudEvent) {
@@ -40,14 +41,25 @@ class Topic
             $event = $event->to_array();
         }
 
-        try {
-            $this->client->post("/publish/{$this->pubsub}/{$this->topic}", $event, $metadata);
+        if ($this->client instanceof DaprClient) {
+            try {
+                $this->client->post("/publish/{$this->pubsub}/{$this->topic}", $event, $metadata);
 
-            $this->client->extra_headers = ['Content-Type: '.$content_type];
+                $this->client->extra_headers = ['Content-Type: ' . $content_type];
 
-            return true;
-        } catch (DaprException) { // @codeCoverageIgnoreStart
-            return false;
-        } // @codeCoverageIgnoreEnd
+                return true;
+            } catch (DaprException) { // @codeCoverageIgnoreStart
+                return false;
+            } // @codeCoverageIgnoreEnd
+        } elseif ($this->client instanceof NewClient) {
+            try {
+                $this->client->publishEvent($this->pubsub, $this->topic, $event, $metadata);
+                return true;
+            } catch (DaprException) {
+                return false;
+            }
+        }
+
+        return false;
     }
 }
