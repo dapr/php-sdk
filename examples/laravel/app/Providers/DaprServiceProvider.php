1b10a8b3d7c30bbade16b06a0e176386841796a6
Rob Landers <landers.robert@gmail.com>
2021-07-25 22:49:57 +0200

    Update laravel example (#107)
    * Include laravel example in auto-update
    
    * Add laravel example to workflows
    
    * Test laravel
    
    * Use pre-built image
    
    * Have a prebuild hook
    
    * Give full path to dockerfile
    
    * Also ensure composer is installed
    
    * Only update examples during build

--- examples/laravel/app/Providers/DaprServiceProvider.php
+++ examples/laravel/app/Providers/DaprServiceProvider.php
@@ -2,14 +2,9 @@
 
 namespace App\Providers;
 
-use Dapr\DaprClient;
+use Dapr\Client\DaprClient;
 use Dapr\Deserialization\DeserializationConfig;
-use Dapr\Deserialization\Deserializer;
-use Dapr\Deserialization\IDeserializer;
-use Dapr\Middleware\Defaults\Tracing;
-use Dapr\Serialization\ISerializer;
 use Dapr\Serialization\SerializationConfig;
-use Dapr\Serialization\Serializer;
 use Illuminate\Support\ServiceProvider;
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
@@ -19,27 +14,12 @@ class DaprServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(
-            ISerializer::class,
-            fn(ContainerInterface $container) => new Serializer(
-                $container->get(SerializationConfig::class),
-                $container->get(LoggerInterface::class)
-            )
-        );
-        $this->app->singleton(
-            IDeserializer::class,
-            fn(ContainerInterface $container) => new Deserializer(
-                $container->get(DeserializationConfig::class),
-                $container->get(LoggerInterface::class)
-            )
-        );
-        $this->app->singleton(
             DaprClient::class,
-            fn(ContainerInterface $container) => new DaprClient(
-                $container->get(LoggerInterface::class),
-                $container->get(IDeserializer::class),
-                $container->get(Tracing::class),
-                config('dapr.port', 3500)
-            )
+            fn(ContainerInterface $container) => DaprClient::clientBuilder()->useHttpClient(
+                "http://localhost:".config('dapr.port', '3500')
+            )->withLogger($container->get(LoggerInterface::class))->withSerializationConfig(
+                $container->get(SerializationConfig::class)
+            )->withDeserializationConfig($container->get(DeserializationConfig::class))->build()
         );
     }
 }
